c524a2024ae206ce91b8bee72ceb48697f0b89d0
igrisdev <johanalvarezz237@gmail.com>
2026-09-01 21:32:54 -0500

    Refactor CrudRepository to implement generic CRUD operations

--- src/main/java/com/std/ec/repository/persistence/CrudRepository.java
+++ src/main/java/com/std/ec/repository/persistence/CrudRepository.java
@@ -4,10 +4,66 @@
  */
 package com.std.ec.repository.persistence;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaQuery;
+import java.util.List;
+import org.hibernate.Criteria;
+
 /**
  *
  * @author igrisdev
+ * @param <T>
  */
-public class CrudRepository {
+public abstract class CrudRepository<T> implements Repository<T> {
+
+    private final Class<T> entityClass;
+
+    public CrudRepository(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    @PersistenceContext(unitName = "CRUD-PU")
+    protected EntityManager em;
+
+    protected abstract EntityManager getEntityManager();
+
+    @Override
+    public T save(T entity) {
+        getEntityManager().persist(entity);
+        return entity;
+    }
+
+    ;
+        
+    @Override
+    public T update(T entity) {
+        getEntityManager().merge(entity);
+        return entity;
+    }
+
+    ;
     
+    @Override
+    public T findById(Object entityId) {
+        return getEntityManager().find(entityClass, entityId);
+    }
+
+    ;
+    
+    @Override
+    public void delete(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
+
+    ; 
+    
+    @Override
+    public List<T> findAll() {
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+;
+
 }
